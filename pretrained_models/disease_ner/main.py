4fb3dad189dfd65fc1936dd2c89794d1b78e1647
EC2 Default User <ec2-user@ip-172-31-66-4.ec2.internal>
2022-10-10 19:44:51 +0000

    <code> Implementation of disease response - one by one

--- pretrained_models/disease_ner/main.py
+++ pretrained_models/disease_ner/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from transformers import pipeline,AutoTokenizer,AutoModelForTokenClassification
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI(title='Disease NER')
+
+tokenizer_final = AutoTokenizer.from_pretrained("models/")
+model_final = AutoModelForTokenClassification.from_pretrained("models/")
+service = pipeline("token-classification",model=model_final,tokenizer=tokenizer_final,aggregation_strategy="simple")
+
+
+class ServiceInput(BaseModel):
+    sentence: str
+
+class ServiceResponse(BaseModel):
+    diseases: List
 
 @app.get("/", tags=["Health Check"])
 async def root():
     return {"message": "Service is online."}
 
 @app.post("/disease_extraction", tags=['Disease NER'])
-async def disease_extraction(input_text: str = ''):
-        return {"message": "Model not Implemented"}
+async def disease_extraction(input_text: ServiceInput):
+    response = service(input_text.sentence)
+    response = [d["word"] for d in response]
+    response_object = ServiceResponse(diseases=response)
+    return response_object
