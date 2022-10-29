efab05bb87ef5dfa7153a20fd95411355fab9b8f
dionnajacobson <dionnajacobson@gmail.com>
2022-10-29 17:11:15 +0000

    fix to AutoModelForSequenceClassification

--- pretrained_models/setfit_icd/get_models.py
+++ pretrained_models/setfit_icd/get_models.py
@@ -1,15 +1,14 @@
-from transformers import AutoModelForTokenClassification, AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
 def get_model():
-    model = AutoModelForTokenClassification.from_pretrained("rjac/setfit-ICD10-L3")
-    tokenizer = AutoTokenizer.from_pretrained("rjac/setfit-ICD10-L3")
-    model.save_pretrained("models/",state_dict=model.state_dict())
+    model = AutoTokenizer.from_pretrained("rjac/setfit-ICD10-L3")
+    tokenizer = AutoModelForSequenceClassification.from_pretrained("rjac/setfit-ICD10-L3")
+    model.save_pretrained("models/")
     tokenizer.save_pretrained("models/")
     
     # save model for trinton server
-    torch.save(model.state_dict(), 'models/pytorch_model.pt')
-
+    #torch.save(model.state_dict(), 'models/pytorch_model.pt')
 
 if __name__ == "__main__":
     get_model()
