58fc43835291237ccba544dcd903522a11f694c2
dionnajacobson <dionnajacobson@gmail.com>
2022-10-31 17:27:34 +0000

    add code for trinton server setfit

---
+++ pretrained_models/setfit_icd/api.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from inference import ICD
+
+
+
+app = FastAPI(title='Top 5 ICD Group Prediction')
+
+
+class ServiceInput(BaseModel):
+    clinical_note: str
+
+
+class ServiceResponse(BaseModel):
+    icd_group: List
+
+
+@app.get("/", tags=["Health Check"])
+async def root():
+    return {"message": "Service is online."}
+
+
+@app.post("/icd_group_prediction", tags=['Top 5 ICD Group Prediction'])
+async def icd_prediction(input: ServiceInput):
+    
+    # run model and get top 5 predictions 
+    model = ICD(triton_url='triton:8002')
+    response = model.predict(input.clinical_note, num_labels=5)
+    response_object = ServiceResponse(icd_group=response)
+    return response_object
+
--- pretrained_models/setfit_icd/get_models.py
+++ pretrained_models/setfit_icd/get_models.py
@@ -1,14 +1,68 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
-def get_model():
+def get_model_huggingface():
     model = AutoTokenizer.from_pretrained("rjac/setfit-ICD10-L3")
     tokenizer = AutoModelForSequenceClassification.from_pretrained("rjac/setfit-ICD10-L3")
     model.save_pretrained("models/")
     tokenizer.save_pretrained("models/")
+
+
+class PyTorch_to_TorchScript(torch.nn.Module):
+    def __init__(self):
+        super(PyTorch_to_TorchScript, self).__init__()
+        self.model = AutoModelForSequenceClassification.from_pretrained('models/', return_dict=False)
+    def forward(self, data, attention_mask=None):
+        return self.model(data, attention_mask)
+
+
+def get_model_triton():
     
-    # save model for trinton server
-    #torch.save(model.state_dict(), 'models/pytorch_model.pt')
+    tokenizer = AutoTokenizer.from_pretrained('models/')
+    text = "subarachnoid hemorrhage scalp laceration service: surgery major surgical or invasive"
+    
+    input_ids = tokenizer.encode(text, return_tensors='pt', max_length=768, padding='max_length', truncation=True)
+
+    mask = input_ids != 1
+    mask = mask.long()
+    
+    pt_model = PyTorch_to_TorchScript().eval()
+    traced_script_module = torch.jit.trace(pt_model, (input_ids, mask))
+    traced_script_module.save('models/1/model.pt')
+
+
+def get_config():
+    configuration = """
+name: "setfit_icd"
+platform: "pytorch_libtorch"
+max_batch_size: 1024
+input [
+ {
+    name: "input_ids"
+    data_type: TYPE_INT32
+    dims: [ 1, 768 ]
+  },
+{
+    name: "attention_mask"
+    data_type: TYPE_INT32
+    dims: [ 1, 768 ]
+  }
+]
+output {
+    name: "out_proj"
+    data_type: TYPE_FP32
+    dims: [ 1, 275 ]
+  }
+"""
+
+    with open('models/config.pbtxt', 'w') as file:
+        file.write(configuration)      
+
 
 if __name__ == "__main__":
-    get_model()
+    get_model_huggingface()
+    get_model_triton()
+    get_config()
+    
+
+
