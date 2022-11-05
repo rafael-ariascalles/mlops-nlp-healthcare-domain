8f930cfe1a2c1ac6b630e55d63ba4fea279745e6
dionnajacobson <dionnajacobson@gmail.com>
2022-11-05 21:53:29 +0000

    add ner to config file

--- app/service.py
+++ app/service.py
@@ -8,10 +8,10 @@ app = FastAPI()
 async def healthcheck():
     setfit_icd_classifier_healthcheck = requests.get(os.getenv('URL_SETFIT_ICD_SERVICE'))
     bert_icd_classifier_healthcheck = requests.get(os.getenv('URL_BERT_ICD_SERVICE'))
-    #disease_token_classifier_healthcheck = requests.get(os.getenv('URl_DISEASE_SERVICE'))
+    disease_token_classifier_healthcheck = requests.get(os.getenv('URl_DISEASE_SERVICE'))
 
     return {
          "setfit_icd_classifier_healthcheck": setfit_icd_classifier_healthcheck.json()
         ,"bert_icd_classifier_healthcheck": bert_icd_classifier_healthcheck.json() 
-     #   ,"disease_token_classifier_healthcheck": disease_token_classifier_healthcheck.json()
+        ,"disease_token_classifier_healthcheck": disease_token_classifier_healthcheck.json()
     }
---
+++ pretrained_models/disease_ner/copy_models_s3.sh
@@ -0,0 +1,2 @@
+aws s3 cp models/ner/config.pbtxt s3://nlp-medical-biller-group-datasets-mlo4/models/disease_ner/config.pbtxt
+aws s3 cp models/ner/1/model.pt s3://nlp-medical-biller-group-datasets-mlo4/models/disease_ner/1/model.pt
