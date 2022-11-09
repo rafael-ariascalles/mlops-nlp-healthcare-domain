e04f03d09610ba4ac4adf32709ce53150ee15c81
dionnajacobson <dionnajacobson@gmail.com>
2022-11-09 04:08:23 +0000

    fix data load

--- scripts/icd_sequence_classifier/upload_icd.py
+++ scripts/icd_sequence_classifier/upload_icd.py
@@ -1,6 +1,6 @@
 import json 
 import pandas as pd
-from datasets import Dataset,load_dataset
+from datasets import Dataset,load_dataset,DatasetDict,concatenate_datasets, load_from_disk
 import os
 from dotenv import load_dotenv
 
@@ -13,21 +13,21 @@ def load_datasets():
     for dataset_num in range(0, 6):
         df = pd.read_csv(f"../../datasets/training_data/dataset_{dataset_num}.csv") 
         df = df[['TEXT', 'ICD10_L3_DESCRIPTION', 'ICD10_GROUP_DESCRIPTION']]
+        df.to_json(f"../../datasets/training_data/train_dataset_{dataset_num}.jsonl",orient="records",lines=True)
+        dataset = load_dataset("json",data_files=f"../../datasets/training_data/train_dataset_{dataset_num}.jsonl", split="train")
+        dataset = dataset.train_test_split(test_size=0.2,seed=7524)
+        dataset.save_to_disk(f"../../datasets/training_data/biobert_training_dataset_{dataset_num}")
+        dataset_eval = dataset['test']
 
         if dataset_num == 0:
-            df.to_json(f"../../datasets/training_data/dataset_{dataset_num}.jsonl",orient="records",lines=True)
-            dataset = load_dataset("json",data_files=f"../../datasets/training_data/dataset_{dataset_num}.jsonl", split="train")
-            dataset.train_test_split(test_size=0.2,seed=7524)
-        
+            dataset_eval.save_to_disk(f"../../datasets/training_data/biobert_eval_dataset_{dataset_num}")
+            
         else:
-            df_prior = pd.read_csv(f"../../datasets/training_data/dataset_{dataset_num-1}.csv") 
-            df_prior = df_prior[['TEXT', 'ICD10_L3_DESCRIPTION', 'ICD10_GROUP_DESCRIPTION']]
-            df_total = pd.concat([df_prior, df])
-            df_total.to_json(f"../../datasets/training_data/dataset_{dataset_num}.jsonl",orient="records",lines=True)
-            dataset = load_dataset("json",data_files=f"../../datasets/training_data/dataset_{dataset_num}.jsonl", split="train")
-            dataset.train_test_split(test_size=0.2,seed=7524)
+            dataset_eval_prior = load_from_disk(f"../../datasets/training_data/biobert_eval_dataset_{dataset_num-1}")
+            dataset_eval_final = concatenate_datasets([dataset_eval, dataset_eval_prior])                                 
+            dataset_eval_final.save_to_disk(f"../../datasets/training_data/biobert_eval_dataset_{dataset_num}")
+       
     
-        dataset.push_to_hub(f"rjac/biobert_pred_dataset_{dataset_num}",max_shard_size="250MB",private=False,token=TOKEN_HF)
 
 
 if __name__ == "__main__":
