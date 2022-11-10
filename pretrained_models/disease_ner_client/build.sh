9d279c541e25009f44874b146504792595b22dbe
rafael-ariascalles <rafael.ariascalles>
2022-11-10 05:27:42 -0600

    <code> implementation of the diasese ner

---
+++ pretrained_models/disease_ner_client/build.sh
@@ -0,0 +1,2 @@
+docker build . -t rjacdevops/disease_ner_client:latest
+docker push rjacdevops/disease_ner_client:latest
---
+++ pretrained_models/disease_ner_client/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from transformers import pipeline,AutoTokenizer,AutoModelForTokenClassification
+from pydantic import BaseModel
+from typing import List
+from inference import TritonTokenClassificationPipeline
+
+app = FastAPI(title='Disease NER')
+
+MODEL_VERSION = "rjac/biobert-ner-diseases-model"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_VERSION)
+triton_url = 'triton-endpoint:8000'
+triton_model_name ="ner"
+service = TritonTokenClassificationPipeline(triton_url=triton_url,triton_model_name=triton_model_name,tokenizer=tokenizer)
+
+class ServiceInput(BaseModel):
+    sentences: List[str]
+
+class ServiceResponse(BaseModel):
+    diseases: List
+
+@app.get("/", tags=["Health Check"])
+async def root():
+    return {"message": "Service is online."}
+
+@app.post("/disease_extraction", tags=['Disease NER'])
+async def disease_extraction(input_text: ServiceInput):
+    response = service(input_text.sentences)
+    print(response)
+    response = [[d["word"] for d in list_ents] for list_ents in response]
+    response_object = ServiceResponse(diseases=response)
+    return response_object
